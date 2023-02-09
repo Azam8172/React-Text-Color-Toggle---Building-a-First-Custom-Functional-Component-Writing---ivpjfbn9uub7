2ffadeb1e3a965617f03dda20a05908b10b34a7a
MUHAMMAD SHAHAJAM <76770736+Azam8172@users.noreply.github.com>
2023-02-09 22:09:12 +0530

    Update App.js

--- src/components/App.js
+++ src/components/App.js
@@ -1,15 +1,45 @@
-import React, {useEffect, useState} from 'react'
-import '../styles/App.css';
+import React, { useState } from 'react';
+import './App.css';
 
-const App = () => {
-//code here 
+function App() {
+  const [isRed, setRed] = useState(true);
+
+  const handleClick = () => {
+    setRed(!isRed);
+  };
+
   return (
-    <div id="main">
-      <p className={} >Newton School</p>
-      <button id='button' onClick={}>Change Style</button>
+    <div className="App">
+      <button id="button" onClick={handleClick}>
+        Toggle Text Color
+      </button>
+      <p className={isRed ? 'red' : 'blue'}>
+        This is some text with dynamic color.
+      </p>
     </div>
-  )
+  );
 }
 
-
 export default App;
+
+
+
+
+
+
+
+// import React, {useEffect, useState} from 'react'
+// import '../styles/App.css';
+
+// const App = () => {
+// //code here 
+//   return (
+//     <div id="main">
+//       <p className={} >Newton School</p>
+//       <button id='button' onClick={}>Change Style</button>
+//     </div>
+//   )
+// }
+
+
+// export default App;
